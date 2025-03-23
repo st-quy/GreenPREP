4cb5d4c0d356433f6aca8941d2ba8c99b5682d9d
Hung <hungltngcd210409@fpt.edu.vn>
2025-03-23 13:52:49 +0700

    feat(GP-88): Implement Microphone Testing Feature

--- src/app/routes/PrivateRoute.jsx
+++ src/app/routes/PrivateRoute.jsx
@@ -31,6 +31,8 @@ const SpeakingTests = lazy(() => import("@pages/speaking/SpeakingTests.jsx"));
 const SpeakingSubmissionSucces = lazy(
   () => import("@pages/speaking/SpeakingSubmissionSuccess.jsx")
 );
+import Layout from "@pages/Layout.jsx";
+import TestingMicrophone from "@features/speaking/ui/TestingMicrophone.jsx";
 
 const PrivateRoute = [
   {
@@ -110,6 +112,30 @@ const PrivateRoute = [
               },
             ],
           },
+          {
+            path: "introduction",
+            element: <Introduction />,
+          },
+          {
+            path: "testing",
+            element: <TestingMicrophone />,
+          },
+        ],
+      },
+      {
+        path: "reading",
+        element: <ReadingLayout />,
+        children: [
+          {
+            path: "intro",
+            element: <IntroReading />,
+          },
+        ],
+      },
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
           {
             path: "grammar",
             element: <GrammarPage />,
