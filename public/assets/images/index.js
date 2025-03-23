d96d7c926051b74cd7fc6f3da7ca684fd5093f60
Hung <hungltngcd210409@fpt.edu.vn>
2025-03-23 10:17:04 +0700

    fix(GP-64): fix logo

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -2,4 +2,5 @@ import GreenPREP from "./GreenPREP.png";
 import GreenPREPLogo from "./GreenPREP_logo.png";
 import HatLogo from "./Logo.png";
 import RejectedImg from "./RejectedImg.png";
+
 export { GreenPREP, GreenPREPLogo, HatLogo, RejectedImg };
