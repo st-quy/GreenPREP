c598d1e863c00f8be9f43503fe1f740906bbfa36
Hieuu <110680846+hieucodegioi@users.noreply.github.com>
2025-03-24 11:53:12 +0700

    update SessionRoutes.jsx

--- src/app/routes/SessionRoutes.jsx
+++ src/app/routes/SessionRoutes.jsx
@@ -14,38 +14,8 @@ const SessionRoutes = {
           path: "introduction",
           element: <ListeningIntroduction />,
         },
-        // Thêm các màn hình con khác của listening ở đây
-        // {
-        //   path: "practice",
-        //   element: <ListeningPractice />,
-        // },
-        // {
-        //   path: "test",
-        //   element: <ListeningTest />,
-        // }
       ],
     },
-    // Các route khác của session
-    // {
-    //   path: "speaking",
-    //   element: <SpeakingLayout />,
-    //   children: [...],
-    // },
-    // {
-    //   path: "reading",
-    //   element: <ReadingLayout />,
-    //   children: [...],
-    // },
-    // {
-    //   path: "writing",
-    //   element: <WritingLayout />,
-    //   children: [...],
-    // },
-    // {
-    //   path: "grammar",
-    //   element: <GrammarLayout />,
-    //   children: [...],
-    // }
   ],
 };
 
