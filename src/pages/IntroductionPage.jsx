f327a19550e7670a5c595bd581b9d3b625e74863
Hung <hungltngcd210409@fpt.edu.vn>
2025-03-24 10:21:57 +0700

    feat(GP-65): fix review

--- src/pages/IntroductionPage.jsx
+++ src/pages/IntroductionPage.jsx
@@ -2,34 +2,9 @@ import { Layout, Typography, Flex, Grid, Button } from "antd";
 const { Header, Content } = Layout;
 const { Title, Text } = Typography;
 import { HatLogo } from "@assets/images";
+import { StepData } from "@shared/contant";
 
-const StepData = [
-  {
-    title: "Speaking",
-    description:
-      "You’ll describe pictures, share experiences, and give opinions to showcase your speaking abilities.",
-  },
-  {
-    title: "Listening",
-    description:
-      "Tune in to various accents and contexts as you answer questions based on dialogues, monologues, and everyday conversations.",
-  },
-  {
-    title: "Grammar & vocabulary",
-    description:
-      "Demonstrate your understanding of English grammar structures and word usage through a series of engaging questions.",
-  },
-  {
-    title: "Reading",
-    description:
-      "Analyze different texts, extract key information, and improve your reading comprehension skills with multiple-choice and short-answer questions.",
-  },
-  {
-    title: "Writing",
-    description:
-      "Conclude your test with a writing task that challenges you to organize your ideas clearly and effectively in written form.",
-  },
-];
+
 const IntroductionPage = () => {
   return (
     <Layout>
