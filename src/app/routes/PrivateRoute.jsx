81d08a48defafe3c11659109aea8f15240ce9557
Thành <vutienthanh250903@gmail.com>
2025-03-24 11:24:27 +0700

    'GP-78 WEB - COMMON - Implement to display question navigation'  delete UI test

--- src/app/routes/PrivateRoute.jsx
+++ src/app/routes/PrivateRoute.jsx
@@ -36,10 +36,6 @@ const PrivateRoute = [
             path: "intro",
             element: <IntroReading />,
           },
-          // {
-          //   path: "navigation",
-          //   element: <NavigationTest />, set the navigation to test module reading with navigation. Test by: reading/navigation 
-          // }
         ],
       },
       {
