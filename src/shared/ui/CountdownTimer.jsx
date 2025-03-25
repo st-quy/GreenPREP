1e15546edb433df87c5db55b743b5817dd0dff95
TBot <TrongThinh2602@gmail.com>
2025-03-25 17:07:27 +0700

    GP-87/GP-77: Fix CountdownTimer display modal

--- src/shared/ui/CountdownTimer.jsx
+++ src/shared/ui/CountdownTimer.jsx
@@ -1,10 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { decrementTime } from "../../app/providers/reducer/timeSlice";
+import { setTime, decrementTime } from "../../app/providers/reducer/timeSlice";
+import ConfirmTestSubmissionModal from "@shared/ui/Modal/ConfirmTestSubmissionModal"; // Import modal
 
-const CountdownTimer = ({ onTimeUp }) => {
+/**
+ * @typedef {Object} RootState
+ * @property {Object} countdown
+ * @property {number} countdown.timeLeft
+ */
+
+
+/*
+  ------Import CountdownTimer--------
+
+    const navigator = useNavigate();
+  
+    const handleSubmitTest = () => {
+      navigator("/session/reading");  //----Setup route submit button for next skill----
+      localStorage.removeItem("countdownTime"); 
+    };
+
+
+    <div>
+      <CountdownTimer initialTime={900} onSubmit={handleSubmitTest} />  //--------Setup time(900= 900s:60=15m | 1800s = 30m.)
+    </div>
+*/
+
+const CountdownTimer = ({ initialTime = 600, onSubmit }) => {
   const dispatch = useDispatch();
-  const timeLeft = useSelector((state) => state.countdown.timeLeft);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  /** @type {number} */
+  const timeLeft = useSelector((/** @type {RootState} */ state) => state.countdown.timeLeft);
+
+  useEffect(() => {
+    if (!localStorage.getItem("countdownTime")) {
+      dispatch(setTime(initialTime));
+    }
+  }, [dispatch, initialTime]);
 
   useEffect(() => {
     if (timeLeft > 0) {
@@ -14,36 +47,40 @@ const CountdownTimer = ({ onTimeUp }) => {
 
       return () => clearInterval(timer);
     } else {
-      onTimeUp();
+      setIsModalVisible(true);
     }
-  }, [timeLeft, dispatch, onTimeUp]);
+  }, [timeLeft, dispatch]);
+
+  const handleSubmit = () => {
+    setIsModalVisible(false);
+    if (onSubmit) {
+      onSubmit();
+    }
+  };
 
   const minutes = Math.floor(timeLeft / 60);
   const seconds = timeLeft % 60;
   const formattedTime = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 
   return (
-    <div className="flex justify-center items-center bg-white w-20 h-10 shadow-md font-medium text-md md:text-lg" style={{ color: "#3758F9" }}>
-      {formattedTime}
+    <div className="relative">
+      <div
+        className="flex justify-center items-center bg-white w-20 h-10 shadow-md font-medium text-md md:text-lg"
+        style={{ color: "#3758F9" }}
+      >
+        {formattedTime}
+      </div>
+
+      <ConfirmTestSubmissionModal
+        visible={isModalVisible}
+        onSubmit={handleSubmit}
+        onCancel={handleSubmit}
+        title="Time is up!"
+        description="Your time has run out. You need to submit your test now."
+        showCancel={false}
+      />
     </div>
   );
 };
 
 export default CountdownTimer;
-
-
-// You can setup <CountdownTimer> as below!
-
-// import CountdownTimer from "../../shared/ui/CountdownTimer";
-
-// const Introduct = () => {
-//   const handleTimeUp = () => {
-//     alert("Time out!");
-//   };
-
-//   return (
-//     <div>
-//       <CountdownTimer onTimeUp={handleTimeUp} />
-//     </div>
-//   );
-// };
