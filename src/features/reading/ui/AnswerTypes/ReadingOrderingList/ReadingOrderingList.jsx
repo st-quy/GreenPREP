2b4a4a4811e86de4b7fd0e507c2bddd6de809c29
LeviTNguyen <romnguyen465@gmail.com>
2025-03-25 17:19:18 +0700

    feat: [GP-114] ReadingOrderinglist

--- src/features/reading/ui/AnswerTypes/ReadingOrderingList/ReadingOrderingList.jsx
+++ src/features/reading/ui/AnswerTypes/ReadingOrderingList/ReadingOrderingList.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
+import { useRef, useState } from "react";
+import { Card, Typography } from "antd";
+import OrderingList from "@shared/ui/OrderingList";
 const ReadingOrderingList = ({ dataSource }) => {
-  return <div>Ordering Code</div>;
+  const formatAnswersData = (data) => {
+    if (data && Array.isArray(data)) {
+      return data.map((item, index) => ({
+        key: item,
+        value: index + 1,
+      }));
+    }
+    return {};
+  };
+  const answers = useRef(formatAnswersData(dataSource));
+  const handleOrderingChange = (data) => {
+    answers.current = formatAnswersData(data);
+    console.log(JSON.stringify(answers.current));
+  };
+  return (
+    <Card className="w-full mx-auto border-none">
+      <div>
+        <OrderingList
+          options={dataSource.AnswerContent.options}
+          onChange={handleOrderingChange}
+        />
+      </div>
+    </Card>
+  );
 };
-
 export default ReadingOrderingList;
