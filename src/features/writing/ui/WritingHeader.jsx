d5173a4dbda6a72d237296a9806a7efcd5f51775
DangTuanGRE <dangviettuangre@gmail.com>
2025-03-25 13:03:45 +0700

    Revert "feat/GP-95/WEB - Implement to view instructions information about the Writing test."
    This reverts commit 53c2d4d164f82dd6b8df0319e951754aa719c4f9.

--- src/features/writing/ui/WritingHeader.jsx
+++ src/features/writing/ui/WritingHeader.jsx
@@ -2,11 +2,11 @@ import { WriteImg } from "@assets/images";
 
 const WritingHeader = () => {
   return (
-    <div className="flex items-center mb-6 mt-0">
-      <div className="w-20 h-20 bg-[#3758F9] rounded-lg flex items-center justify-center mr-3">
-        <img src={WriteImg} alt="Writing Test Logo" className="w-[54px] h-[54px]" />
+    <div className="flex items-center mb-6 mt-6">
+      <div className="w-12 h-12 bg-[#3758F9] rounded-lg flex items-center justify-center mr-3">
+        <img src={WriteImg} alt="Writing Test Logo" className="w-6 h-6" />
       </div>
-      <h1 className="text-[40px] leading-[48px] font-bold">Writing Test</h1>
+      <h1 className="text-2xl font-bold">Writing Test</h1>
     </div>
   );
 };
