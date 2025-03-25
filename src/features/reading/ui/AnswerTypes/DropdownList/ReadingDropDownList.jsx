faf47c8aee5dbcd454ce1b7f2812d845be9070d9
TBot <TrongThinh2602@gmail.com>
2025-03-25 16:48:44 +0700

    feat/GP-94: Fix timer

--- src/features/reading/ui/AnswerTypes/DropdownList/ReadingDropDownList.jsx
+++ src/features/reading/ui/AnswerTypes/DropdownList/ReadingDropDownList.jsx
@@ -5,10 +5,53 @@ const ReadingDropdownList = ({ dataSource }) => {
     return <h1>Dropdown Inline</h1>;
   }
 
+  console.log(dataSource);
+
   return (
-    <div>
-      <h1>Dropdown Part 3</h1>
-    </div>
+    <div className="text-sm md:text-base">
+        {dataSource.AnswerContent.content && (
+          <div className="mb-6">
+            {dataSource.AnswerContent.content.split("\n").map((line, index) => {
+              if (!line.includes(":")) {
+                return (
+                  <p key={index} className="text-black">
+                    {line}
+                  </p>
+                );
+              }
+  
+              const parts = line.split(":");
+              return (
+                <p key={index} className="text-black my-2">
+                  <span className="font-bold text-black underline">{parts[0]}</span>:
+                  <span className="text-black">{parts.slice(1).join(":")}</span>
+                </p>
+              );
+            })}
+          </div>
+        )}
+  
+        {dataSource.AnswerContent.leftItems.map((question, idx) => (
+          <div key={idx} className="flex items-center my-4 justify-between">
+            <label className="text-black">{question}</label>
+            <select
+              key={idx}
+              className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 mx-2"
+              value={dataSource.userAnswers?.[idx] || ""}
+              onChange={(e) => dataSource.handleAnswerChange(idx, e.target.value)}
+            >
+              <option value="">Select answer</option>
+              {Array.isArray(dataSource.AnswerContent.rightItems) && dataSource.AnswerContent.rightItems.length > 0
+                ? dataSource.AnswerContent.rightItems.map((option, i) => (
+                    <option key={i} value={option}>
+                      {option}
+                    </option>
+                  ))
+                : <option value="" disabled>No options available</option>}
+            </select>
+          </div>
+        ))}
+      </div>
   );
 };
 
--- src/features/reading/ui/Sidebar/Sidebar.jsx
+++ src/features/reading/ui/Sidebar/Sidebar.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import CountdownTimer from "@shared/ui/CountdownTimer";
 import RQuestionNavigator from "../ReadingNavigator";
-
-const handleTimeUp = () => {
-  alert("Time out!");
-};
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigator = useNavigate();
+
+  const handleSubmitTest = () => {
+    navigator("/session/reading");
+    localStorage.removeItem("countdownTime"); 
+  };
+
   return (
     <div className="order-1 rounded-[20px] border border-black border-opacity-30 bg-white p-6 shadow-xl sticky top-0 md:w-[300px] lg:w-[300px] lg:h-[500px] lg:p-8">
       <h6 className="mb-4 text-[16px] font-semibold">Time Remaining</h6>
-      {/* <CountdownTimer onTimeUp={handleTimeUp} /> */}
+      <CountdownTimer initialTime={3000} onSubmit={handleSubmitTest} />
       {/* <RQuestionNavigator /> */}
     </div>
   );
