f9e8983fa17e29567f81771c2f97081a27b3288b
NguyenVanThangzzzz <106645703+NguyenVanThangzzzz@users.noreply.github.com>
2025-03-26 14:28:44 +0700

    fix navigate success

--- src/features/writing/ui/WritingSuccess.jsx
+++ src/features/writing/ui/WritingSuccess.jsx
@@ -1,10 +1,16 @@
 import SubmissionSuccess from "@shared/SubmissionSuccess/SubmissionSuccess";
-
+import { useNavigate } from "react-router-dom";
 
 const WritingPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-        <SubmissionSuccess onNextTest={null} testType = "writing" isLastTest={true} />
+        <SubmissionSuccess 
+        onNextTest={() => navigate('/')}
+        testType="writing" 
+        isLastTest={true} 
+        />
     </div>
   );
 };
