c0c415db81929540849ad6427fa94447fd4c03e0
nhat nguyen <vannhat.nguyen@stunited.vn>
2025-04-22 16:52:26 +0700

    add docker file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "",
+  base: "/",
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
   resolve: {
     alias: {
       "@": path.resolve("./src"),
